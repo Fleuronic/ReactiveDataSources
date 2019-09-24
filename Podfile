b0daadbe39d08ad382a1b9df2e5920faf8028b65
Daniel Ostashev <daniil.ostashev@cruxlab.com>
2019-09-24 17:31:00 +0300

    Rearrange files, add ReactiveCocoa

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'ReactiveDataSources' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for ReactiveDataSources
+
+  pod 'ReactiveCocoa', '~> 10.0'
+  pod 'RxSwift', '~> 5.0'
+  pod 'RxCocoa', '~> 5.0'
+  pod 'Differentiator', '~> 4.0'  
+
+  target 'ReactiveDataSourcesTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ReactiveDataSources/ReactiveDataSources.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveDataSources.h
+//  ReactiveDataSources
+//
+//  Created by Daniel Ostashev on 9/24/19.
+//  Copyright © 2019 Cruxlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactiveDataSources.
+FOUNDATION_EXPORT double ReactiveDataSourcesVersionNumber;
+
+//! Project version string for ReactiveDataSources.
+FOUNDATION_EXPORT const unsigned char ReactiveDataSourcesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveDataSources/PublicHeader.h>
+
+
